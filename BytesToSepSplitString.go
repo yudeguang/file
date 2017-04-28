69adf6766cefabf73ec897607d54f0639bf3be0d
yudeguang <84558040@qq.com>
2017-04-28 09:11:37 +0800

    Signed-off-by: yudeguang <84558040@qq.com>

--- BytesToSepSplitString.go
+++ BytesToSepSplitString.go
@@ -60,6 +60,61 @@ func BytesToSepSplitString(data []byte, definition []string, sep string) string 
 	return strings.Join(arr, sep)
 }
 
+//处理时增加了去除末尾的空格
+func BytesToSepSplitStringTrimRight(data []byte, definition []string, sep string) string {
+	if sep == "" {
+		log.Fatal("sep分隔符不能为空")
+	}
+	arr := []string{}
+	for _, v := range definition {
+		//前面两位表示顺序，仅为了便于观察，这里并不需要处理这两位
+		//第三位中，0表示不读取,s表示读取STRING，I表示读取数字类型
+		str := ""
+		dataType := v[2:3]
+		dataLen, _ := strconv.Atoi(v[3:])
+		switch dataType {
+		case "0":
+			_, data = FetchString(data, dataLen)
+		case "s":
+			str, data = FetchString(data, dataLen)
+		case "i":
+			switch dataLen {
+			case 1:
+				temp := uint8(0)
+				temp, data = FetchUINT8(data)
+				str = strconv.Itoa(int(temp))
+			case 2:
+				temp := uint16(0)
+				temp, data = FetchUINT16(data)
+				str = strconv.Itoa(int(temp))
+			case 4:
+				temp := uint32(0)
+				temp, data = FetchUINT32(data)
+				str = strconv.Itoa(int(temp))
+			case 8:
+				temp := uint64(0)
+				temp, data = FetchUINT64(data)
+				str = strconv.Itoa(int(temp))
+			default:
+				log.Fatal("definition中i的数据类型长度范围必须是1,2,4,8中的其中一种,而当前长度为:", dataLen)
+			}
+
+		default:
+			log.Fatal("definition中i的数据类型必须是s,i,0其中的一种，而当前长度为:", dataType)
+		}
+		if strings.Contains(str, sep) {
+			log.Fatal("sep分隔符的选择不符合规范，与目标数据有冲突")
+		}
+		str = strings.TrimRight(str, " ")
+		arr = append(arr, str)
+	}
+	//上述过程处理完，如果data中还有数据，那么说明定义有问题
+	if len(data) != 0 {
+		log.Fatal("definition定义有误，请检察")
+	}
+	return strings.Join(arr, sep)
+}
+
 /*
 definition定义方法:definition := []string{"01s69", "02i2", "0304"}。其中:
 1.前两位,01,02,03,表示序列，防止definition定义得太长时看不清楚前后关系。
